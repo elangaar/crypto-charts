7f211458f73b88b5fe2ba6eda5f5cbd9e41983f4
elangaar <mikolaj.kowal@yahoo.pl>
2022-06-10 05:13:55 +0200

    Separate get_chart function from main function + refactoring

--- crypto_charts/coins.py
+++ crypto_charts/coins.py
@@ -1,7 +1,6 @@
 """Script for comparing cryptocurrency charts from different sources."""
 import json
 import datetime
-
 import pandas as pd
 import plotly.express as px
 import plotly.utils
@@ -19,17 +18,17 @@ klines_urls = {
     'Binance': '/api/v3/klines',
     'FTX': '/markets/{market_name}/candles'
 }
-ex_pair_separator = {
+exchanges_pair_separator = {
     'KuCoin': '-',
     'Binance': '',
     'FTX': '/'
 }
-ex_interval_phrase = {
+exchanges_interval_phrase = {
     'KuCoin': 'type',
     'Binance': 'interval',
     'FTX': 'resolution'
 }
-ex_time_phrase = {
+exchanges_timephrase = {
     'start': {
         'KuCoin': 'startAt',
         'Binance': 'startTime',
@@ -41,12 +40,12 @@ ex_time_phrase = {
         'FTX': 'end_time'
     }
 }
-ex_timeframes = {
+exchanges_timeframes = {
     'KuCoin': '1day',
     'Binance': '1d',
     'FTX': '86400'
 }
-ex_factor = {
+exchanges_factors = {
     'KuCoin': 1,
     'Binance': 1000,
     'FTX': 1
@@ -61,20 +60,21 @@ def get_exchanges_list(ex):
 
 def get_symbol(coin1, coin2, ex):
     """Return currency pair symbol."""
-    return ex_pair_separator.get(ex).join((coin1.upper(), coin2.upper()))
+    return exchanges_pair_separator.get(ex).join((coin1.upper(), coin2.upper()))
 
 def get_market(s, symbol):
+    """Return the market part of the url string"""
     if '{' in s and '}' in s:
         return s.format(market_name=symbol) + '?'
     return f'{s}?symbol={symbol}&'
 
-def get_data_url(coin1, coin2, ex, tf, start_time=datetime.datetime.now()-datetime.timedelta(days=60), end_time=datetime.datetime.now(), symbol=None) :
+def get_data_url(coin1, coin2, ex, tf, start_time=datetime.datetime.now()-datetime.timedelta(days=10), end_time=datetime.datetime.now(), symbol=None) :
     """"Return the URL to an endpoint with data from the exchange."""
     symbol = get_symbol(coin1, coin2, ex)
-    start_time = str(int(float(datetime.datetime.timestamp(start_time)) * ex_factor.get(ex)))
-    end_time = str(int(float(datetime.datetime.timestamp(end_time)) * ex_factor.get(ex)))
-    url_string = f"{exchanges_urls.get(ex)}{get_market(klines_urls.get(ex), symbol)}{ex_interval_phrase.get(ex)}={tf}&"\
-            f"{ex_time_phrase.get('start')[ex]}={start_time}&{ex_time_phrase.get('end')[ex]}={end_time}"
+    start_time = str(int(float(datetime.datetime.timestamp(start_time)) * exchanges_factors.get(ex)))
+    end_time = str(int(float(datetime.datetime.timestamp(end_time)) * exchanges_factors.get(ex)))
+    url_string = f"{exchanges_urls.get(ex)}{get_market(klines_urls.get(ex), symbol)}{exchanges_interval_phrase.get(ex)}={tf}&"\
+            f"{exchanges_timephrase.get('start')[ex]}={start_time}&{exchanges_timephrase.get('end')[ex]}={end_time}"
     return url_string
 
 def get_kucoin_data(data):
@@ -104,11 +104,12 @@ def get_binance_data(data):
     return data
 
 def get_ftx_data(data):
+    """Return statistics data and data chart from FTX exchange."""
     if data.get('success') is True:
         if len(data.get('result')) > 0:
             df = pd.DataFrame({
                 'Czas': [datetime.datetime.strptime(t.get('startTime'), '%Y-%m-%dT%H:%M:%S%z') +
-                         datetime.timedelta(hours=2, seconds=int(ex_timeframes.get('FTX'))) for t in data.get('result')],
+                         datetime.timedelta(hours=2, seconds=int(exchanges_timeframes.get('FTX'))) for t in data.get('result')],
                 'Cena zamknięcia': [p.get('close') for p in data.get('result')],
                 'Giełda': ['FTX' for i in range(len(data.get('result')))]
             })
@@ -116,42 +117,44 @@ def get_ftx_data(data):
         return 'Brak danych dla tego okresu.'
     return data
 
+def get_chart(data, symbol):
+    """Return a chart string to display on the page by plotly"""
+    fig = px.line(data, x='Czas', y='Cena zamknięcia', color='Giełda', title=f'{symbol} chart')
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
 @bp.route('/', methods=['GET', 'POST'])
-def get_chart():
+def main():
     """Return the price chart for a selected pair of cryptocurrencies from selected exchanges."""
-    content = {'data': None}
-    content['exchanges_list'] = get_exchanges_list(exchanges_urls)
+    content = {
+        'exchanges_list': get_exchanges_list(exchanges_urls)
+    }
     if request.method == 'POST':
         chart_data = pd.DataFrame(columns=['Czas', 'Cena zamknięcia', 'Giełda'])
-        exchanges = request.form.getlist('ex')
+        exchanges = request.form.getlist('exchanges')
         error = ''
-        for ex in exchanges:
+        for exchange in exchanges:
             params = {
-                'symbol': get_symbol(request.form['coin1'], request.form['coin2'], request.form['ex']),
+                'symbol': get_symbol(request.form['coin1'], request.form['coin2'], request.form['exchanges']),
                 'coin1': request.form['coin1'],
                 'coin2': request.form['coin2'],
-                'tf': ex_timeframes[ex]
+                'tf': exchanges_timeframes[exchange]
             }
-            data_url = get_data_url(**params, ex=ex)
-            print(data_url)
+            data_url = get_data_url(**params, ex=exchange)
             r = requests.get(data_url)
             data = r.json()
-            ex_check = {
+            exchange_check = {
                 'KuCoin': get_kucoin_data,
                 'Binance': get_binance_data,
                 'FTX': get_ftx_data
             }
-            ret = ex_check.get(ex)(data)
-            if isinstance(ret, pd.DataFrame):
-                chart_data = pd.concat([chart_data, ret], ignore_index=True)
+            data_prepared = exchange_check.get(exchange)(data)
+            if isinstance(data_prepared, pd.DataFrame):
+                chart_data = pd.concat([chart_data, data_prepared], ignore_index=True)
             else:
-                error = ret
+                error = data_prepared
         if not chart_data.empty:
-            fig = px.line(chart_data, x='Czas', y='Cena zamknięcia', color='Giełda', title=f'{params["symbol"]} chart')
-            content['graph_json'] = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-            content['data'] = {
-                'symbol': params['symbol'],
-                'ex': exchanges
-            }
+            content['graph_json'] = get_chart(chart_data, params['symbol'])
+            content['symbol'] = params['symbol']
+            content['exchanges'] = exchanges
         flash(error)
     return render_template('index.html', **content)
